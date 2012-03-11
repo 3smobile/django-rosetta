8561a1f49a038d4a90a53cad01416fe0a246da5e
Will Boyce <me@willboyce.com>
2012-03-11 22:53:19 +0000

    also handle mobile.po files

--- rosetta/poutil.py
+++ rosetta/poutil.py
@@ -37,19 +37,19 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
                     continue
             cache.set('rosetta_django_paths', django_paths, 60*60)
         paths = paths + django_paths
-        
-    
-    # settings 
+
+
+    # settings
     for localepath in settings.LOCALE_PATHS:
         if os.path.isdir(localepath):
             paths.append(localepath)
-    
+
     # project/app/locale
     for appname in settings.INSTALLED_APPS:
-                
+
         if rosetta_settings.EXCLUDED_APPLICATIONS and appname in rosetta_settings.EXCLUDED_APPLICATIONS:
             continue
-            
+
         p = appname.rfind('.')
         if p >= 0:
             app = getattr(__import__(appname[:p], {}, {}, [appname[p+1:]]), appname[p+1:])
@@ -57,7 +57,7 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
             app = __import__(appname, {}, {}, [])
 
         apppath = os.path.normpath(os.path.abspath(os.path.join(os.path.dirname(app.__file__), 'locale')))
-        
+
 
         # django apps
         if 'contrib' in apppath and 'django' in apppath and not django_apps:
@@ -66,18 +66,18 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
         # third party external
         if not third_party_apps and abs_project_path not in apppath:
             continue
-            
+
         # local apps
         if not project_apps and abs_project_path in apppath:
             continue
-            
-        
+
+
         if os.path.isdir(apppath):
             paths.append(apppath)
-            
-            
-        
-            
+
+
+
+
     ret = set()
     langs = (lang,)
     if u'-' in lang:
@@ -86,12 +86,12 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
     elif u'_' in lang:
         _l,_c = map(lambda x:x.lower(),lang.split(u'_'))
         langs += (u'%s-%s' %(_l, _c), u'%s-%s' %(_l, _c.upper()), )
-        
+
     paths = map(os.path.normpath, paths)
     for path in paths:
         for lang_ in langs:
             dirname = os.path.join(path, lang_, 'LC_MESSAGES')
-            for fn in ('django.po','djangojs.po',):
+            for fn in ('django.po','djangojs.po','mobile.po'):
                 filename = os.path.join(dirname, fn)
                 if os.path.isfile(filename):
                     ret.add(os.path.abspath(filename))
@@ -99,19 +99,19 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
 
 def pagination_range(first,last,current):
     r = []
-    
+
     r.append(first)
     if first + 1 < last: r.append(first+1)
-    
+
     if current -2 > first and current -2 < last: r.append(current-2)
     if current -1 > first and current -1 < last: r.append(current-1)
     if current > first and current < last: r.append(current)
     if current + 1 < last and current+1 > first: r.append(current+1)
     if current + 2 < last and current+2 > first: r.append(current+2)
-    
+
     if last-1 > first: r.append(last-1)
     r.append(last)
-    
+
     r = list(set(r))
     r.sort()
     prev = 10000
