67a20e1e3fa194eafd3ca23f14caf58561d9ca89
Will Boyce <me@willboyce.com>
2012-03-15 03:24:04 +0000

    also handle dbslugs.po files

--- rosetta/poutil.py
+++ rosetta/poutil.py
@@ -91,7 +91,7 @@ def find_pos(lang, project_apps=True, django_apps=False, third_party_apps=False)
     for path in paths:
         for lang_ in langs:
             dirname = os.path.join(path, lang_, 'LC_MESSAGES')
-            for fn in ('django.po','djangojs.po','mobile.po'):
+            for fn in ('django.po','djangojs.po','mobile.po', 'dbslugs.po'):
                 filename = os.path.join(dirname, fn)
                 if os.path.isfile(filename):
                     ret.add(os.path.abspath(filename))
